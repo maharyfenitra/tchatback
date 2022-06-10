fa7a5be2af93e26dda4f81a55684386499a995e7
Mahary R <mrafanomezana@bocasay.com>
2022-06-10 16:23:42 +0300

    Suppression commentaire inutile

--- index.js
+++ index.js
@@ -4,18 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-/*
-const mongoose = require('mongoose');
-mongoose.connect(process.env.URL_DB);
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected')
-})*/
 
 const { usersRouter } = require("./routes/users.js")
 const { messagesRouter } = require("./routes/messages.js")
